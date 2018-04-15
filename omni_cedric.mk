c1a8cb3edf2591a66d7b0978e81f867d38346f23
Oves Shaikh <30339311+oves786@users.noreply.github.com>
2018-04-15 12:26:15 +0530

    Update omni_cedric.mk

--- omni_cedric.mk
+++ omni_cedric.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-$(call inherit-product, device/motorola/cedric/full_cedric.mk)
+$(call inherit-product, device/motorola/cedric/full_pd1613.mk)
 
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := omni_cedric
+PRODUCT_NAME := omni_pd1613
